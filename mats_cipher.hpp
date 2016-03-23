29881560f35dd473b160cb93d1e058773fe935fe
Mamat Rahmat <coderbodoh@gmail.com>
2016-03-24 00:21:01 +0700

    add inverse s-box

--- mats_cipher.hpp
+++ mats_cipher.hpp
@@ -15,6 +15,7 @@ class mats_cipher{
 	private:
 		vector<string> s_blocks;
 		static const unsigned char s_box[256];
+		static const unsigned char s_box_reverse[256];
 
 	public: 
 		bool isCBC, isCFB;
@@ -26,8 +27,8 @@ class mats_cipher{
 		string single_encipher(string s, string key);
 		string single_decipher(string s, string key);
 		
-		string do_modified_feistel(string p, string key);
-		string sub_bytes(string s);
+		string do_modified_feistel(string p, string key, bool encrypt);
+		string sub_bytes(string s, bool encrypt);
 		string get_sha256(string s);
 		string convert_256_to_128(string s);
 
